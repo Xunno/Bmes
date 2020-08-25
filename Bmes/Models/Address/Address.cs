2f24eb7bfe0128e3b52b7d84d2b6f36835cf05c4
惠昭 冯 <1142974782@qq.com>
2020-08-25 09:31:19 +0800

    update

--- Bmes/Models/Address/Address.cs
+++ Bmes/Models/Address/Address.cs
@@ -4,6 +4,12 @@ namespace Bmes.Models.Address
 {
     public class Address :BaseObject
     {
- 
+        //Name
+        //AddressLine1
+        //AddressLine2
+        //City
+        //Country
+        //State
+        //Zipcode
     }
 }
