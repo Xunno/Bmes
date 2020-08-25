4ee506752c40cf53d93bb41073d82f09033981b6
wsw0811 <3066470476@qq.com>
2020-08-25 10:01:03 +0800

    update

--- Bmes/Models/Address/Address.cs
+++ Bmes/Models/Address/Address.cs
@@ -4,6 +4,12 @@ namespace Bmes.Models.Address
 {
     public class Address :BaseObject
     {
- 
+        public string Name { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public int Zipcode { get; set; }
     }
 }
--- Bmes/Models/Product/Brand.cs
+++ Bmes/Models/Product/Brand.cs
@@ -5,5 +5,6 @@ namespace Bmes.Models.Product
     public class Brand : BaseObject
     {
         public string Name { get; set; }
+        public string Id { get; set; }
     }
 }
